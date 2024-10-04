a24c1ce2abeaf943025993c73e0682eb2aefdaef
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-10-04 13:16:59 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -15,7 +15,7 @@ export default defineConfig({
     tsconfigPaths(),
     // Allows using self-signed certificates to run the dev server using HTTPS.
     // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
-    basicSsl(),
+    // basicSsl(),
 		svgr(),
   ],
   server: {
