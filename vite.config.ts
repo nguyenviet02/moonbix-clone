3cfa99e38120a988a7367faae0e9e60c8c857ace
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-10-04 14:37:45 +0700

    config gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 // import basicSsl from '@vitejs/plugin-basic-ssl';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/moonbix-clone',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
@@ -16,11 +17,10 @@ export default defineConfig({
     // Allows using self-signed certificates to run the dev server using HTTPS.
     // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
     // basicSsl(),
-		svgr(),
+    svgr(),
   ],
   server: {
     // Exposes your dev server and makes it accessible for the devices in the same network.
     host: true,
   },
 });
-
