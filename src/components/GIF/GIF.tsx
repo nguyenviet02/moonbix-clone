5a0f7e9721d76ed30f8079a6a159c74c7eb601e0
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-10-07 11:29:39 +0700

    fix: type in GIF component

--- src/components/GIF/GIF.tsx
+++ src/components/GIF/GIF.tsx
@@ -1,6 +1,27 @@
 import { useEffect, useMemo, useRef } from 'react';
 import { Image } from 'react-konva';
+import { Image as ImageType } from 'konva/lib/shapes/Image';
 import 'gifler';
+
+declare global {
+  interface Window {
+    gifler: (src: string) => {
+      get: (callback: (a: Animator2) => void) => void;
+    };
+  }
+}
+
+type TFrame = {
+  buffer: CanvasImageSource;
+  x: number;
+  y: number;
+};
+
+type Animator2 = {
+  animateInCanvas: (canvas: HTMLCanvasElement) => void;
+  stop: () => void;
+  onDrawFrame: (ctx: CanvasRenderingContext2D, frame: TFrame) => void;
+};
 
 type GIFProps = {
   src: string;
@@ -9,7 +30,7 @@ type GIFProps = {
 };
 
 const GIF = ({ src, x, y }: GIFProps) => {
-  const imageRef = useRef(null);
+  const imageRef = useRef<ImageType | null>(null);
   const canvas = useMemo(() => {
     const node = document.createElement('canvas');
     return node;
@@ -17,13 +38,14 @@ const GIF = ({ src, x, y }: GIFProps) => {
 
   useEffect(() => {
     // save animation instance to stop it on unmount
-    let anim;
-    window.gifler(src).get((a) => {
+    let anim: Animator2;
+    window.gifler(src).get((a: Animator2) => {
       anim = a;
+      console.log('☠️ ~ useEffect ~ anim:', anim);
       anim.animateInCanvas(canvas);
       anim.onDrawFrame = (ctx, frame) => {
         ctx.drawImage(frame.buffer, frame.x, frame.y);
-        imageRef.current.getLayer().draw();
+        imageRef?.current?.getLayer?.()?.draw();
       };
     });
     return () => anim.stop();
